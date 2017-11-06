84cd5be01199393cb2b1b75c1bc098e22b7ff497
ukapure <ukapure@sfu.ca>
2017-11-06 10:27:36 -0800

    initial chef config. TODO: db migrations/connection

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,4 +1,17 @@
 # Make sure the Apt package lists are up to date, so we're downloading versions that exist.
+
+execute 'get gpg key' do 
+  command 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg'
+end 
+
+execute 'add key to trusted' do 
+  command 'sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg'
+end 
+
+execute 'move dotnet package to apt-sources' do 
+  command "sudo sh -c 'echo \"deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main\" > /etc/apt/sources.list.d/dotnetdev.list'"
+end 
+
 cookbook_file "apt-sources.list" do
   path "/etc/apt/sources.list"
 end
@@ -14,4 +27,23 @@ cookbook_file "ntp.conf" do
 end
 execute 'ntp_restart' do
   command 'service ntp restart'
+end
+
+execute 'get dotnet' do 
+  command 'apt-get install dotnet-sdk-2.0.0 -y'
+end 
+
+execute 'get deps' do
+  command 'dotnet restore'
+  cwd '/home/ubuntu/project'
+end 
+# TODO skip db stuff for now, just get hello world running
+# execute 'run migrations' do 
+#   command 'dotnet ef database update'
+#   cwd '/home/ubuntu/project'
+# end 
+
+execute 'start dotnet' do 
+  command 'nohup dotnet run > /dev/null 2>&1 &'
+  cwd '/home/ubuntu/project'
 end
