58ad1862e3119611dcca510ccbcbc29176ffc077
Ushma <ukapure@sfu.ca>
2017-11-08 10:31:49 -0800

    install postgres & nginx. create db mydb

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,21 +1,20 @@
 # Make sure the Apt package lists are up to date, so we're downloading versions that exist.
 
-execute 'get gpg key' do 
+execute 'get gpg key' do
   command 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg'
-end 
+end
 
-execute 'add key to trusted' do 
+execute 'add key to trusted' do
   command 'mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg'
-end 
+end
 
-execute 'move dotnet package to apt-sources' do 
+execute 'move dotnet package to apt-sources' do
   command "sh -c 'echo \"deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main\" > /etc/apt/sources.list.d/dotnetdev.list'"
-end 
+end
 
-execute 'add npm set up' do 
-  user 'ubuntu'
+execute 'add npm set up' do
   command 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -'
-end 
+end
 
 cookbook_file "apt-sources.list" do
   path "/etc/apt/sources.list"
@@ -27,40 +26,47 @@ end
 # Base configuration recipe in Chef.
 package "wget"
 package "ntp"
+package "nginx"
+package "postgresql"
+
 cookbook_file "ntp.conf" do
   path "/etc/ntp.conf"
 end
+
 execute 'ntp_restart' do
   command 'service ntp restart'
 end
 
-execute 'get dotnet' do 
+execute 'create db' do
+  command 'echo "CREATE DATABASE mydb; CREATE USER ubuntu; GRANT ALL PRIVILEGES ON DATABASE mydb TO ubuntu;" | sudo -u postgres psql'
+end
+
+execute 'get dotnet' do
   command 'apt-get install dotnet-sdk-2.0.0 -y'
-end 
+end
 
 execute 'get dotnet deps' do
   command 'dotnet restore'
   cwd '/home/ubuntu/project'
-end 
+end
 
-execute 'get node' do 
+execute 'get node' do
   command 'apt-get install -y nodejs'
-end 
-
+end
 
 execute 'get node deps' do
   command 'npm install'
   cwd '/home/ubuntu/project'
-end 
-
+end
 
 # TODO skip db stuff for now, just get hello world running
-# execute 'run migrations' do 
+# execute 'run migrations' do
 #   command 'dotnet ef database update'
 #   cwd '/home/ubuntu/project'
-# end 
+# end
 
-execute 'start dotnet' do 
-  command 'nohup dotnet run > /dev/null 2>&1 &'
-  cwd '/home/ubuntu/project'
-end
+# TODO uncomment this -- autoruns dotnet
+# execute 'start dotnet' do
+#   command 'nohup dotnet run > /dev/null 2>&1 &'
+#   cwd '/home/ubuntu/project'
+# end
