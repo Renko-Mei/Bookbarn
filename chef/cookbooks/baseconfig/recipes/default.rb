6013e287b7a9df4f9220ba321388b329eac1be47
xwa132 <xwa132@sfu.ca>
2017-11-17 22:45:24 -0800

    Change to autorun in vagrant

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -80,10 +80,10 @@ execute 'Update db for other models' do
 end
 
 # TODO uncomment this -- autoruns dotnet
-# execute 'start dotnet' do
-#   command 'nohup dotnet run > /dev/null 2>&1 &'
-#   cwd '/home/ubuntu/project'
-# end
+execute 'start dotnet' do
+  command 'nohup dotnet run > /dev/null 2>&1 &'
+  cwd '/home/ubuntu/project'
+end
 
 execute 'start nginx' do
   command 'service nginx start'
