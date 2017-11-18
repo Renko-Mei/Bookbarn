6dbdd53ca98044eb087656af2759275ec013d9be
xwa132 <xwa132@sfu.ca>
2017-11-17 22:33:42 -0800

    Delete unncessary nav

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -23,16 +23,6 @@ export class NavMenu extends React.Component<{}, {}> {
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to={ '/counter' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-education'></span> Counter
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/fetchdata' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Fetch data
-                            </NavLink>
-                        </li>
-                        <li>
                             <NavLink to={'/search'} activeClassName='active'>
                                 <span className='glyphicon glyphicon-th-list'></span> Search
                             </NavLink>
