adce3347819ef294ef49cdc4384e9917f29e24f6
Ushma <ukapure@sfu.ca>
2017-11-19 16:59:21 -0800

    some cleanup

---
+++ ClientApp/components/SaleItem.tsx
@@ -0,0 +1,12 @@
+import * as  React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+
+export default class SaleItem extends React.Component<RouteComponentProps<{}>, {}>{
+  public render (){
+    return <div>
+      <h1> testing! </h1>
+      </div>;
+  }
+
+}
