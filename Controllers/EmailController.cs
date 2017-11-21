7888a22f16d68ce683ccfd70d0544d588ca56b98
markq <markq@sfu.ca>
2017-11-21 12:30:19 -0800

    email now can get users email and the function has been optimized

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -15,45 +15,55 @@ namespace BookBarn.Controllers
     {
 
         private readonly AuthenticationContext _context;
-        //private readonly Data.InitialModelsContext _icontext;
 
         public EmailController(AuthenticationContext context)
         {
             _context = context;
         }
-        public string X()
+
+        public string EmailInfo()
         {
-            //string userEmail = _context.Users.SingleOrDefaultAsync(cw => cw.UserName == User.Identity.Name).Result.Email;
-            string userEmail = _context.Users.FirstOrDefault(c => c.UserName == User.Identity.Name).Email;
-                                     
+            string userEmail = _context.Users.FirstOrDefault(c => c.UserName == User.Identity.Name).Email;                         
             return userEmail;
         }
 
-
-
+        public string NameInfo()
+        {
+            return _context.Users.FirstOrDefault(c => c.UserName == User.Identity.Name).UserName;
+        }
 
         [HttpGet]
         public IActionResult Index()
         {
-            
-
-            ViewData["userEmail"] = X();
+            ViewData["userEmail"] = EmailInfo();
+            //ViewData["userName"] = NameInfo();
 
             return View("Email_Input");
         }
+
         [HttpPost]
         //[ValidateAntiForgeryToken]
-        public IActionResult Index(string customerEmail, string customerRequest)
+        public IActionResult Index(string customerEmail, string customerRequest, string customerPhoneNumber)
         {
-
+            string customerName = "Customer Name: " + NameInfo();
+            //string customerDetail = "CustomerName: " + customerName;
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("BookBarn", "info@bookbarncanada.com"));
-            message.To.Add(new MailboxAddress("mark", customerEmail));
+            message.To.Add(new MailboxAddress("mark", customerEmail));//this should be changed to seller email
             message.Subject = "test mail in asp.net core";
-            message.Body = new TextPart("plain")
-            {
-                Text = customerRequest
-            };
+            var builder = new BodyBuilder();
+            builder.TextBody = @"Dear seller,";
+            if(customerPhoneNumber ==null){
+                customerPhoneNumber = "not provided";
+            }
+            builder.HtmlBody = "<em>"+customerName+"<br>Customer Email: "+customerEmail+ "<br>Customer Phone Number: "+ customerPhoneNumber + "</em><br><br>Customer concern is: <br>"+customerRequest;
+
+            message.Body = builder.ToMessageBody();
+            //message.Body = new TextPart("plain")
+            //{
+            //    Text = "CustomerName: " + customerName +
+            //        customerRequest
+            //};
             using (var client = new SmtpClient())
             {
                 client.Connect("smtp.gmail.com", 587, false);
