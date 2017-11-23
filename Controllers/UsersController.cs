21ae28aa583909e263c81babc592ecbac945a33d
acyao <acyao@sfu.ca>
2017-11-22 20:23:33 -0800

    Added sales viz, chartjs library,

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
 using BookBarn.Models.IdentityViewModels;
 using Microsoft.AspNetCore.Authentication;
+using ChartJSCore.Models;
 
 namespace BookBarn.Controllers
 {
@@ -140,6 +141,45 @@ namespace BookBarn.Controllers
             {
                 return RedirectToAction(nameof(HomeController.Index), "Home");
             }
+        }
+
+        public IActionResult SalesVisualization(){
+            Chart chart = new Chart();
+            chart.Type = "line";
+            ChartJSCore.Models.Data data = new ChartJSCore.Models.Data();
+            data.Labels = new List<string>() { "January", "February", "March", "April", "May", "June", "July" };
+            LineDataset dataset = new LineDataset()
+            {
+                Label = "My First dataset",
+                Data = new List<double>() { 65, 59, 80, 81, 56, 55, 40 },
+                Fill = false,
+                LineTension = 0.1,
+                BackgroundColor = "rgba(75, 192, 192, 0.4)",
+                BorderColor = "rgba(75,192,192,1)",
+                BorderCapStyle = "butt",
+                BorderDash = new List<int> { },
+                BorderDashOffset = 0.0,
+                BorderJoinStyle = "miter",
+                PointBorderColor = new List<string>() { "rgba(75,192,192,1)" },
+                PointBackgroundColor = new List<string>() { "#fff" },
+                PointBorderWidth = new List<int> { 1 },
+                PointHoverRadius = new List<int> { 5 },
+                PointHoverBackgroundColor = new List<string>() { "rgba(75,192,192,1)" },
+                PointHoverBorderColor = new List<string>() { "rgba(220,220,220,1)" },
+                PointHoverBorderWidth = new List<int> { 2 },
+                PointRadius = new List<int> { 1 },
+                PointHitRadius = new List<int> { 10 },
+                SpanGaps = false
+            };
+
+            data.Datasets = new List<Dataset>();
+            data.Datasets.Add(dataset);
+
+            chart.Data = data;
+
+            ViewData["chart"] = chart;
+
+            return View();
         }
     }
 }
