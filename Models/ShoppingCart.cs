0cce1bbc29163f1101f6d92bc49f2f565ed2dd48
Ushma <ukapure@sfu.ca>
2017-11-23 11:18:07 -0800

    Shopping cart scaffolded

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BookBarn.Models
+{
+  public class ShoppingCart
+  {
+    public int ShoppingCartId { get; set; }
+    public int BuyerId { get; set; }
+    public List<SaleItem> SaleItems { get; set; }
+  }
+}
+
+
+//dotnet aspnet-codegenerator controller -name ShoppingCartsController -m ShoppingCart -dc InitialModelsContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
