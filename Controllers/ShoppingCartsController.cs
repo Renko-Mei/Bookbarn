c319d07e4b300d03d0af0d8db92b3c98bc6ec304
acyao <acyao@sfu.ca>
2017-11-30 13:26:29 -0800

    Implemented shopping cart, need test

--- Controllers/ShoppingCartsController.cs
+++ Controllers/ShoppingCartsController.cs
@@ -7,155 +7,188 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BookBarn.Data;
 using BookBarn.Models;
+using BookBarn.Models.ShoppingCartViewModel;
 
 namespace BookBarn.Controllers
 {
     public class ShoppingCartsController : Controller
     {
         private readonly InitialModelsContext _context;
+        private readonly ShoppingCart _shoppingCart;
 
-        public ShoppingCartsController(InitialModelsContext context)
+
+        public ShoppingCartsController(InitialModelsContext context, ShoppingCart shoppingCart)
         {
             _context = context;
+            _shoppingCart = shoppingCart;
         }
 
-        // GET: ShoppingCarts
+        // // GET: ShoppingCarts
         public async Task<IActionResult> Index()
         {
-            return View(await _context.ShoppingCart.ToListAsync());
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var shoppingCartView = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartView);
+            //return View(await _context.ShoppingCart.ToListAsync());
         }
 
-        // GET: ShoppingCarts/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public RedirectToActionResult AddToShoppingCart(int saleItemId)
         {
-            if (id == null)
-            {
-              Response.StatusCode = 404;
-              return View("NotFound");
-            }
+            var selectedItem = _context.SaleItem.FirstOrDefault(p => p.SaleItemId == saleItemId);
 
-            var shoppingCart = await _context.ShoppingCart
-                .SingleOrDefaultAsync(m => m.ShoppingCartId == id);
-            if (shoppingCart == null)
+            if(selectedItem!=null)
             {
-              Response.StatusCode = 404;
-              return View("NotFound");
+                _shoppingCart.AddToCart(selectedItem, 1);
             }
-
-            return View(shoppingCart);
+            return RedirectToAction("Index");
         }
 
-        // GET: ShoppingCarts/Create
-        public IActionResult Create()
+        public RedirectToActionResult RemoveFromShoppingCart(int saleItemId)
         {
-            return View();
-        }
+            var selectedItem = _context.SaleItem.FirstOrDefault(p => p.SaleItemId == saleItemId);
 
-        // POST: ShoppingCarts/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ShoppingCartId")] ShoppingCart shoppingCart)
-        {
-            if (ModelState.IsValid)
+            if(selectedItem!=null)
             {
-                _context.Add(shoppingCart);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(shoppingCart);
+                _shoppingCart.RemoveFromCart(selectedItem);
+            }   
+            return RedirectToAction("Index");
         }
+        // // GET: ShoppingCarts/Details/5
+        // public async Task<IActionResult> Details(int? id)
+        // {
+        //     if (id == null)
+        //     {
+        //       Response.StatusCode = 404;
+        //       return View("NotFound");
+        //     }
 
-        // GET: ShoppingCarts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-              Response.StatusCode = 404;
-              return View("NotFound");
-            }
+        //     var shoppingCart = await _context.ShoppingCart
+        //         .SingleOrDefaultAsync(m => m.ShoppingCartId == id);
+        //     if (shoppingCart == null)
+        //     {
+        //       Response.StatusCode = 404;
+        //       return View("NotFound");
+        //     }
 
-            var shoppingCart = await _context.ShoppingCart.SingleOrDefaultAsync(m => m.ShoppingCartId == id);
-            if (shoppingCart == null)
-            {
-              Response.StatusCode = 404;
-              return View("NotFound");
-            }
-            return View(shoppingCart);
-        }
+        //     return View(shoppingCart);
+        // }
 
-        // POST: ShoppingCarts/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ShoppingCartId")] ShoppingCart shoppingCart)
-        {
-            if (id != shoppingCart.ShoppingCartId)
-            {
-              Response.StatusCode = 404;
-              return View("NotFound");
-            }
+        // // GET: ShoppingCarts/Create
+        // public IActionResult Create()
+        // {
+        //     return View();
+        // }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(shoppingCart);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ShoppingCartExists(shoppingCart.ShoppingCartId))
-                    {
-                      Response.StatusCode = 404;
-                      return View("NotFound");
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(shoppingCart);
-        }
+        // // POST: ShoppingCarts/Create
+        // // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> Create([Bind("ShoppingCartId")] ShoppingCart shoppingCart)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         _context.Add(shoppingCart);
+        //         await _context.SaveChangesAsync();
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     return View(shoppingCart);
+        // }
 
-        // GET: ShoppingCarts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-              Response.StatusCode = 404;
-              return View("NotFound");
-            }
+        // // GET: ShoppingCarts/Edit/5
+        // public async Task<IActionResult> Edit(int? id)
+        // {
+        //     if (id == null)
+        //     {
+        //       Response.StatusCode = 404;
+        //       return View("NotFound");
+        //     }
 
-            var shoppingCart = await _context.ShoppingCart
-                .SingleOrDefaultAsync(m => m.ShoppingCartId == id);
-            if (shoppingCart == null)
-            {
-              Response.StatusCode = 404;
-              return View("NotFound");
-            }
+        //     var shoppingCart = await _context.ShoppingCart.SingleOrDefaultAsync(m => m.ShoppingCartId == id);
+        //     if (shoppingCart == null)
+        //     {
+        //       Response.StatusCode = 404;
+        //       return View("NotFound");
+        //     }
+        //     return View(shoppingCart);
+        // }
 
-            return View(shoppingCart);
-        }
+        // // POST: ShoppingCarts/Edit/5
+        // // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> Edit(int id, [Bind("ShoppingCartId")] ShoppingCart shoppingCart)
+        // {
+        //     if (id != shoppingCart.ShoppingCartId)
+        //     {
+        //       Response.StatusCode = 404;
+        //       return View("NotFound");
+        //     }
 
-        // POST: ShoppingCarts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var shoppingCart = await _context.ShoppingCart.SingleOrDefaultAsync(m => m.ShoppingCartId == id);
-            _context.ShoppingCart.Remove(shoppingCart);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //     if (ModelState.IsValid)
+        //     {
+        //         try
+        //         {
+        //             _context.Update(shoppingCart);
+        //             await _context.SaveChangesAsync();
+        //         }
+        //         catch (DbUpdateConcurrencyException)
+        //         {
+        //             if (!ShoppingCartExists(shoppingCart.ShoppingCartId))
+        //             {
+        //               Response.StatusCode = 404;
+        //               return View("NotFound");
+        //             }
+        //             else
+        //             {
+        //                 throw;
+        //             }
+        //         }
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     return View(shoppingCart);
+        // }
 
-        private bool ShoppingCartExists(int id)
-        {
-            return _context.ShoppingCart.Any(e => e.ShoppingCartId == id);
-        }
+        // // GET: ShoppingCarts/Delete/5
+        // public async Task<IActionResult> Delete(int? id)
+        // {
+        //     if (id == null)
+        //     {
+        //       Response.StatusCode = 404;
+        //       return View("NotFound");
+        //     }
+
+        //     var shoppingCart = await _context.ShoppingCart
+        //         .SingleOrDefaultAsync(m => m.ShoppingCartId == id);
+        //     if (shoppingCart == null)
+        //     {
+        //       Response.StatusCode = 404;
+        //       return View("NotFound");
+        //     }
+
+        //     return View(shoppingCart);
+        // }
+
+        // // POST: ShoppingCarts/Delete/5
+        // [HttpPost, ActionName("Delete")]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> DeleteConfirmed(int id)
+        // {
+        //     var shoppingCart = await _context.ShoppingCart.SingleOrDefaultAsync(m => m.ShoppingCartId == id);
+        //     _context.ShoppingCart.Remove(shoppingCart);
+        //     await _context.SaveChangesAsync();
+        //     return RedirectToAction(nameof(Index));
+        // }
+
+        // private bool ShoppingCartExists(int id)
+        // {
+        //     return _context.ShoppingCart.Any(e => e.ShoppingCartId == id);
+        // }
     }
 }
