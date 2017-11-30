cb044270c0907eeb3c16f624145e07b1bb6f183a
markq <markq@sfu.ca>
2017-11-29 23:45:43 -0800

    now search and saleItem works

--- Controllers/SaleItemsController.cs
+++ Controllers/SaleItemsController.cs
@@ -13,6 +13,9 @@ using Microsoft.AspNetCore.Authorization;
 using System.IO;
 using Microsoft.AspNetCore.Http;
 using BookBarn.Utilities;
+using MailKit.Net.Smtp;
+using MimeKit;
+
 
 namespace BookBarn.Controllers
 {
@@ -374,5 +377,61 @@ namespace BookBarn.Controllers
             ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
             return View();
         }
+
+         public string EmailInfo()
+        {
+            string userEmail = _Acontext.Users.FirstOrDefault(c => c.UserName == User.Identity.Name).Email;                         
+            return userEmail;
+        }
+
+        public string NameInfo()
+        {
+            return _Acontext.Users.FirstOrDefault(c => c.UserName == User.Identity.Name).UserName;
+        }
+
+        // [HttpGet]
+        // public IActionResult Email()
+        // {
+        //     ViewData["userEmail"] = EmailInfo();
+        //     //ViewData["SellerEmail"] = ;
+
+        //     return View("Email_Input");
+        // }
+
+        // [HttpPost]
+        // //[ValidateAntiForgeryToken]
+        // public IActionResult Email(string customerEmail, string customerRequest, string customerPhoneNumber)
+        // {
+            
+        //     string customerName = "Customer Name: " + NameInfo();
+        //     //string customerDetail = "CustomerName: " + customerName;
+        //     var message = new MimeMessage();
+        //     message.From.Add(new MailboxAddress("BookBarn", "info@bookbarncanada.com"));
+        //     message.To.Add(new MailboxAddress("mark", customerEmail));//this should be changed to seller email
+        //     message.Subject = "test mail in asp.net core";
+        //     var builder = new BodyBuilder();
+        //     builder.TextBody = @"Dear seller, you have received an email request from your customer";
+        //     if(customerPhoneNumber ==null){
+        //         customerPhoneNumber = "not provided";
+        //     }
+        //     builder.HtmlBody = "<em>"+customerName+"<br>Customer Email: "+customerEmail+ "<br>Customer Phone Number: "+ customerPhoneNumber + "</em><br><br>Customer concern is: <br>"+customerRequest;
+
+        //     message.Body = builder.ToMessageBody();
+        //     //message.Body = new TextPart("plain")
+        //     //{
+        //     //    Text = "CustomerName: " + customerName +
+        //     //        customerRequest
+        //     //};
+        //     using (var client = new SmtpClient())
+        //     {
+        //         client.Connect("smtp.gmail.com", 587, false);
+        //         client.Authenticate("info@bookbarncanada.com", "InfoBookBarn");
+        //         client.Send(message);
+
+        //         client.Disconnect(true);
+        //     }
+
+        //     return View("Index");
+        // }
     }
 }
