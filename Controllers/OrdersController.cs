08eb8bc3dc60bf721de281a9550b341a6f2d0772
acyao <acyao@sfu.ca>
2017-12-03 15:03:27 -0800

    Checkout now works

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -59,14 +59,16 @@ namespace BookBarn.Controllers
                         IsSold = true,
                         BuyerId = name,
                         SellerId = sellerId,
-                        SaleItems = new List<SaleItem>()
+                        SaleItems = new List<SaleItem>(),
+                        SalePrice = 0
                     };
 
                     foreach(var saleItems in seller)
                     {
                         var newSaleItem = saleItems.SaleItem;
                         if(newSaleItem != null)
-                        {
+                        {   
+                            newOrder.SalePrice += newSaleItem.Price;
                             newOrder.SaleItems.Add(newSaleItem);
                         }
                     }
@@ -84,144 +86,5 @@ namespace BookBarn.Controllers
             ViewBag.CheckoutComplete = "Thank you for ordering from BookBarn!";
             return View();
         }
-
-        // // GET: Orders
-        // public async Task<IActionResult> Index()
-        // {
-        //     return View(await _context.Order.ToListAsync());
-        // }
-
-        // // GET: Orders/Details/5
-        // public async Task<IActionResult> Details(int? id)
-        // {
-        //     if (id == null)
-        //     {
-        //       Response.StatusCode = 404;
-        //       return View("NotFound");
-        //     }
-
-        //     var order = await _context.Order
-        //         .SingleOrDefaultAsync(m => m.OrderId == id);
-        //     if (order == null)
-        //     {
-        //       Response.StatusCode = 404;
-        //       return View("NotFound");
-        //     }
-
-        //     return View(order);
-        // }
-
-        // // GET: Orders/Create
-        // public IActionResult Create()
-        // {
-        //     return View();
-        // }
-
-        // // POST: Orders/Create
-        // // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public async Task<IActionResult> Create([Bind("OrderId,SalePrice,OrderDate,ShippedDate,IsSold,BuyerId,SellerId")] Order order)
-        // {
-        //     if (ModelState.IsValid)
-        //     {
-        //         _context.Add(order);
-        //         await _context.SaveChangesAsync();
-        //         return RedirectToAction(nameof(Index));
-        //     }
-        //     return View(order);
-        // }
-
-        // // GET: Orders/Edit/5
-        // public async Task<IActionResult> Edit(int? id)
-        // {
-        //     if (id == null)
-        //     {
-        //       Response.StatusCode = 404;
-        //       return View("NotFound");
-        //     }
-
-        //     var order = await _context.Order.SingleOrDefaultAsync(m => m.OrderId == id);
-        //     if (order == null)
-        //     {
-        //       Response.StatusCode = 404;
-        //       return View("NotFound");
-        //     }
-        //     return View(order);
-        // }
-
-        // // POST: Orders/Edit/5
-        // // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public async Task<IActionResult> Edit(int id, [Bind("OrderId,SalePrice,OrderDate,ShippedDate,IsSold,BuyerId,SellerId")] Order order)
-        // {
-        //     if (id != order.OrderId)
-        //     {
-        //       Response.StatusCode = 404;
-        //       return View("NotFound");
-        //     }
-
-        //     if (ModelState.IsValid)
-        //     {
-        //         try
-        //         {
-        //             _context.Update(order);
-        //             await _context.SaveChangesAsync();
-        //         }
-        //         catch (DbUpdateConcurrencyException)
-        //         {
-        //             if (!OrderExists(order.OrderId))
-        //             {
-        //               Response.StatusCode = 404;
-        //               return View("NotFound");
-        //             }
-        //             else
-        //             {
-        //                 throw;
-        //             }
-        //         }
-        //         return RedirectToAction(nameof(Index));
-        //     }
-        //     return View(order);
-        // }
-
-        // // GET: Orders/Delete/5
-        // public async Task<IActionResult> Delete(int? id)
-        // {
-        //     if (id == null)
-        //     {
-        //       Response.StatusCode = 404;
-        //       return View("NotFound");
-        //     }
-
-        //     var order = await _context.Order
-        //         .SingleOrDefaultAsync(m => m.OrderId == id);
-        //     if (order == null)
-        //     {
-        //       Response.StatusCode = 404;
-        //       return View("NotFound");
-        //     }
-
-        //     return View(order);
-        // }
-
-        // // POST: Orders/Delete/5
-        // [HttpPost, ActionName("Delete")]
-        // [ValidateAntiForgeryToken]
-        // public async Task<IActionResult> DeleteConfirmed(int id)
-        // {
-        //     var order = await _context.Order.SingleOrDefaultAsync(m => m.OrderId == id);
-        //     _context.Order.Remove(order);
-        //     await _context.SaveChangesAsync();
-        //     return RedirectToAction(nameof(Index));
-        // }
-
-        // private bool OrderExists(int id)
-        // {
-        //     return _context.Order.Any(e => e.OrderId == id);
-        // }
     }
 }
--- Models/Order.cs
+++ Models/Order.cs
@@ -7,30 +7,6 @@ namespace BookBarn.Models
 {
   public class Order
   {
-    //private readonly InitialModelsContext _context;
-    //private readonly ShoppingCart _shoppingCart;
-
-    //may not need ShoppingCart logic 
-    // public Order()
-    // {
-    //     _context = context;
-    //     //_shoppingCart = shoppingCart;
-    // }
-
-    // can be deleted, logic moved to controller
-    // public void CreateOrder(Order order){
-    //   //var shoppingCartItems = _shoppingCart.ShoppingCartItems;
-    //   order.OrderDate = DateTime.Now;
-    //   var shoppingCartItems =  _shoppingCart.ShoppingCartItems;
-
-    //   foreach(var item in shoppingCartItems)
-    //   {
-    //     order.SaleItems.Add(item.SaleItem);    
-    //   }
-
-    //   _context.Order.Add(order);
-    //   _context.SaveChanges();
-    // }
     public int OrderId { get; set; }
     public float SalePrice { get; set; }
     public DateTime OrderDate { get; set; }
